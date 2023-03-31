48b1fd23692b22ee426cdb572829bd331c4ad166
Sandhy <tiwarisandhyprakash@gmail.com>
2023-04-01 02:15:21 +0530

    four

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -31,7 +31,7 @@ public class Payment {
         this.id = id;
     }
 
-    public boolean getPaymentCompleted() {
+    public boolean isPaymentCompleted() {
         return paymentCompleted;
     }
 
